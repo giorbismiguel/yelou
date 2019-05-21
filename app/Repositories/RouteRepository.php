020e9b78e956d1a0d1d7058bec46663d2d5a8560
Giorbis <giorbismiguel@gmail.com>
2019-05-21 18:08:53 -0400

    Implement routes in api

---
+++ app/Repositories/RouteRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Route;
+use App\Repositories\BaseRepository;
+
+/**
+ * Class RouteRepository
+ * @package App\Repositories
+ * @version May 21, 2019, 8:52 pm UTC
+*/
+
+class RouteRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'name',
+        'lat',
+        'lng',
+        'formatted_address'
+    ];
+
+    /**
+     * Return searchable fields
+     *
+     * @return array
+     */
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return Route::class;
+    }
+}
---
+++ tests/Traits/MakeRouteTrait.php
@@ -0,0 +1,53 @@
+<?php namespace Tests\Traits;
+
+use Faker\Factory as Faker;
+use App\Models\Route;
+use App\Repositories\RouteRepository;
+
+trait MakeRouteTrait
+{
+    /**
+     * Create fake instance of Route and save it in database
+     *
+     * @param array $routeFields
+     * @return Route
+     */
+    public function makeRoute($routeFields = [])
+    {
+        /** @var RouteRepository $routeRepo */
+        $routeRepo = \App::make(RouteRepository::class);
+        $theme = $this->fakeRouteData($routeFields);
+        return $routeRepo->create($theme);
+    }
+
+    /**
+     * Get fake instance of Route
+     *
+     * @param array $routeFields
+     * @return Route
+     */
+    public function fakeRoute($routeFields = [])
+    {
+        return new Route($this->fakeRouteData($routeFields));
+    }
+
+    /**
+     * Get fake data of Route
+     *
+     * @param array $routeFields
+     * @return array
+     */
+    public function fakeRouteData($routeFields = [])
+    {
+        $fake = Faker::create();
+
+        return array_merge([
+            'name' => $fake->word,
+            'lat' => $fake->word,
+            'lng' => $fake->word,
+            'formatted_address' => $fake->word,
+            'created_at' => $fake->date('Y-m-d H:i:s'),
+            'updated_at' => $fake->date('Y-m-d H:i:s')
+        ], $routeFields);
+    }
+}
