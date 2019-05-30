633910843b9807b911b8ad1cfe25303d21c6b06e
Giorbis <giorbismiguel@gmail.com>
2019-05-30 19:53:14 -0400

    Add services and professional

---
+++ resources/js/components/pages/dashboard/Professional.vue
@@ -0,0 +1,18 @@
+<template>
+    <section>
+        Professional
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "Professional"
+    }
+</script>
+
+<style scoped>
+    section {
+        width: 1116px;
+        height: 428px;
+    }
+</style>
---
+++ resources/js/components/pages/dashboard/Services.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        Services
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "Services"
+    }
+</script>
+
+<style scoped>
+    section {
+        width: 1116px;
+        height: 406px;
+        background: rgb(245, 252, 255);
+        border-radius: 0px 0px 0px 0px;
+    }
+</style>
