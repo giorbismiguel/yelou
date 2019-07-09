e33ed67bfede57139b8e5b123c08a7e9219b4b42
Giorbis <giorbismiguel@gmail.com>
2019-07-08 23:38:55 -0400

    Change name deleteRequestService by deleteRequestedService

--- resources/js/components/pages/administration/services/Index.vue
+++ resources/js/components/pages/administration/services/Index.vue
@@ -204,7 +204,7 @@
 
         methods: {
             ...mapActions([
-                'deleteRequestedService',
+                'deleteRequestService',
                 'acceptDriverService'
             ]),
 
@@ -230,7 +230,7 @@
                     showLoaderOnConfirm: true
                 }).then((result) => {
                     if (result.value) {
-                        this.deleteRequestedService(id)
+                        this.deleteRequestService(id)
                             .then(() => {
                                 this.loading = false
                                 this.$notify({
