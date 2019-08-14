f8c79aa422011af50fd621e78de518031337132e
Giorbis <giorbismiguel@gmail.com>
2019-08-14 13:53:58 -0400

    Hidden several created_at, updated_at and phone_verified_at for error in carbon

--- app/User.php
+++ app/User.php
@@ -50,6 +50,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'created_at',
+        'updated_at',
+        'phone_verified_at',
     ];
 
     /**
