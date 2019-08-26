b0c7ee7c7991eb192c634630cbd1e2365acd4a58
Giorbis <giorbismiguel@gmail.com>
2019-08-26 14:06:11 -0400

    Try to fix error with HEREDOC

--- resources/lang/es/app.php
+++ resources/lang/es/app.php
@@ -1,4 +1,8 @@
 <?php
+$theDistanceTravel = 'El cliente desea trasladarse desde <strong> :init_address</strong> hasta <strong>:end_address';
+$theDistanceTravel .= '</strong>, la distancia a recorrer es <strong>:distance</strong>';
+$theDistanceTravel .= ' en un tiempo aproximado de <strong>:time</strong> horas a una velocidad de <strong>';
+$theDistanceTravel .= ':velocity</strong>. La tarifa es <strong>:tariff</strong>.';
 
 return [
     'message_code_activation'                        => 'Yelou le informa que su código de activación para su cuenta es :code',
@@ -6,10 +10,7 @@ return [
     'message_code_activation_action'                 => 'Ir a Yelou para autenticarse e introducir el código de activación',
     'message_code_activation_line2'                  => 'Si no se registro en Yelou, no es necesario realizar ninguna otra acción.',
     'customer_request_transportation'                => 'Un cliente a realizado una solicitud de taxi',
-    'the_distance_travel'                            => <<<EOF
- El cliente desea trasladarse desde <strong> :init_address</strong> hasta <strong>:end_address</strong>, la distancia a recorrer es <strong>:distance</strong> 
- en un tiempo aproximado de <strong>:time</strong> horas a una velocidad de <strong>:velocity</strong>. La tarifa es <strong>:tariff</strong>.
-EOF,
+    'the_distance_travel'                            => $theDistanceTravel,
     'want_accept_request'                            => 'Aceptar el servicio',
     'click_ok_provide_transportation_service_client' => <<<EOF
 De clic en Aceptar para prestar el servicio de transportación al cliente.
