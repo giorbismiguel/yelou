58ae6ea0895693eaf706af8aa9fe24676c3189e0
Giorbis Miguel <giorbismiguel@gmail.com>
2019-10-17 17:59:21 -0400

    Organization code

--- app/Http/Requests/API/CreateUserAPIRequest.php
+++ app/Http/Requests/API/CreateUserAPIRequest.php
@@ -30,28 +30,28 @@ class CreateUserAPIRequest extends APIRequest
     public function rules()
     {
         $rules = [
-            'name' => 'required|max:191',
-            'email' => 'required|email|max:191|unique:users',
-            'password' => 'required|min:6|confirmed',
+            'name'       => 'required|max:191',
+            'email'      => 'required|email|max:191|unique:users',
+            'password'   => 'required|min:6|confirmed',
             'first_name' => 'required|max:191',
-            'last_name' => 'required|max:191',
-            'phone' => 'required|max:191|unique:users',
-            'ruc' => 'required|max:191',
-            'direction' => 'nullable|max:191',
+            'last_name'  => 'required|max:191',
+            'phone'      => 'required|max:191|unique:users',
+            'ruc'        => 'required|max:191',
+            'direction'  => 'nullable|max:191',
         ];
 
-        if ((int)request()->get('type') === self::CLIENT) {
+        if ((int) request()->get('type') === self::CLIENT) {
             $rules += [
-                'city' => 'required|max:191',
+                'city'        => 'required|max:191',
                 'postal_code' => 'nullable|max:191',
             ];
 
-        } elseif ((int)request()->get('type') === self::TRANSPORTATION) {
+        } elseif ((int) request()->get('type') === self::TRANSPORTATION) {
             $rules += [
-                'license_types_id' => 'required|integer',
-                'photo' => 'required|image|max:100000',
-                'image_driver_license' => 'required|image|max:100000',
-                'image_permit_circulation' => 'required|image|max:100000',
+                'license_types_id'             => 'required|integer',
+                'photo'                        => 'required|image|max:100000',
+                'image_driver_license'         => 'required|image|max:100000',
+                'image_permit_circulation'     => 'required|image|max:100000',
                 'image_certificate_background' => 'nullable|image|max:100000',
             ];
         }
@@ -62,20 +62,20 @@ class CreateUserAPIRequest extends APIRequest
     public function attributes()
     {
         return [
-            'name' => 'Nombre de usuario',
-            'email' => 'Correo electrónico',
-            'password' => 'Contraseña',
-            'first_name' => 'Nombre ',
-            'last_name' => 'Apellido ',
-            'phone' => 'Teléfono',
-            'ruc' => 'RUC',
-            'direction' => 'Dirección',
-            'city' => 'Ciudad',
-            'postal_code' => 'Código Postal',
-            'license_types_id' => 'Tipo de licencia de conducir',
-            'photo' => 'Foto',
-            'image_driver_license' => 'Foto de la licencia de conducir',
-            'image_permit_circulation' => 'Foto del Permiso de circulación',
+            'name'                         => 'Nombre de usuario',
+            'email'                        => 'Correo electrónico',
+            'password'                     => 'Contraseña',
+            'first_name'                   => 'Nombre ',
+            'last_name'                    => 'Apellido ',
+            'phone'                        => 'Teléfono',
+            'ruc'                          => 'RUC',
+            'direction'                    => 'Dirección',
+            'city'                         => 'Ciudad',
+            'postal_code'                  => 'Código Postal',
+            'license_types_id'             => 'Tipo de licencia de conducir',
+            'photo'                        => 'Foto',
+            'image_driver_license'         => 'Foto de la licencia de conducir',
+            'image_permit_circulation'     => 'Foto del Permiso de circulación',
             'image_certificate_background' => 'Foto del Certificado de Antecedentes',
         ];
     }
